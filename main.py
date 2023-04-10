bfa34b08d35945d421c84024feb1bb8036846d2d
Harsha Evuri <harsha.evuri@gmail.com>
2023-04-10 18:13:42 +0100

    updated python code

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def hello_world3(request):
-    return "Hello, World3!"
+import json
+import requests
+from flask import jsonify
+
+def fetch_data_option3_cf(request):
+    url = 'https://jsonplaceholder.typicode.com/todos/1'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Failed to fetch data"}), response.status_code
